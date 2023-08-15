aa32061b835ad2d109ecf2e50834a22c0d80268a
egmp7 <deweyerick@gmail.com>
2023-08-14 23:43:44 -0500

    languages init

--- app/page.js
+++ app/page.js
@@ -8,20 +8,40 @@ import styles from './page.module.css'
 import Footer from './components/Footer'
 import React from 'react';
 
-const data = {
-  test1: "testing1",
-  test2: "testing 2"
+const english = {
+  slogan:`Chef for epicurean clients`,
+  intro: `I'm a personal chef with more than 15 years of experience, 
+  I studied and graduated from New York Culinary School in 1986. 
+  I also worked in New York at Vista International Hotel at World 
+  Trade Center in the 80’s and Ritz Carlton Hotels for 5 years.. 
+  I have a extensive resume of very high end clientele including Robert 
+  Downey Jr. , Jennifer López, Graig Robbins, Dj Tiesto and others. 
+  Just go to my website Personal Chef Ramone.`,
+  aboutButton: `About Me`,
+  menuButton: `Sample Menu`,
+  contactButton: `Contact`,
+  ratesButton: `Rates and Services`
+}
+
+const spanish ={
+  slogan: `Spanish slogan`,
+  intro:`Spanish intro`,
+  aboutButton: `Sobre mí`,
+  menuButton: `Menú de muestra`,
+  contactButton: `Contacto`,
+  ratesButton: `Tarifas y servicios`
+
 }
 
 
-
-let isEnglish = true;
-
-
 export default function Home() {
-  const [likes, setLikes] = React.useState("Hello");
+  const [language, setLanguage]= React.useState("english")
+  const [pageData, setPageData] = React.useState(english);
   return (
     <main>
+
+      <button  onClick={changeLanguage}>Change Language</button>
+
       <h1>Ramone Personal Chef</h1>
 
       <Image
@@ -35,40 +55,31 @@ export default function Home() {
       width={280}
       height={180}/>
 
-      <p>Chef for epicurean clients</p>
-
-      <p>I'm a personal chef with more than 15 years of experience, 
-        I studied and graduated from New York Culinary School in 1986. 
-        I also worked in New York at Vista International Hotel at World 
-        Trade Center in the 80’s and Ritz Carlton Hotels for 5 years.. 
-        I have a extensive resume of very high end clientele including Robert 
-        Downey Jr. , Jennifer López, Graig Robbins, Dj Tiesto and others. 
-        Just go to my website Personal Chef Ramone.
-      </p>
-
-      <p>{likes}</p>
-      {isEnglish && <p>{likes}</p> || <p>In Spanish</p>}
+      <p>{pageData.slogan}</p>
+      <p>{pageData.intro}</p>
       
-      <button  onClick={changeLanguage}>Change Language</button>
-
-      <Link href="/test">{data.test1}</Link>
-      <Link href="/test">{data.test2}</Link>
-      <Link href="/test">Rates and services</Link>
-      <Link href="/test">Contact</Link>
-      
+      <Link href="/test">{pageData.aboutButton}</Link>
+      <Link href="/test">{pageData.menuButton}</Link>
+      <Link href="/test">{pageData.ratesButton}</Link>
+      <Link href="/test">{pageData.contactButton}</Link>
       <Footer/>
 
-      
-
-
-      
     </main>
   )
+
   function changeLanguage(){
   
-    isEnglish = !isEnglish
-    setLikes("Not hello")
-    console.log(isEnglish)
+    // if english do spanish
+    if (language === "english") {
+      setLanguage("spanish")
+      setPageData(spanish)
+    }
+
+    // if spanish do english
+    if (language === "spanish") {
+      setLanguage("english")
+      setPageData(english)
+    }
   }
   
 }
