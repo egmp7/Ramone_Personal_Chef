cf9f03a4ed88f3a5b8be53e54a717117b756267d
egmp7 <deweyerick@gmail.com>
2023-08-15 13:37:54 -0500

    internationalization init

--- app/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div>
-            <span>Tel: xxx - xxx - xxxx</span>
-            <span>Mail: xxxxxx@gmail.com</span>
-        </div>
-    )}
