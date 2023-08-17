9b18b74ef9dbb720673c6a0207493836cf28a130
egmp7 <deweyerick@gmail.com>
2023-08-17 14:34:36 -0500

    about page intl messages

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -1,22 +1,32 @@
 'use client';
 
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
 import Link from "next/link";
 import Image from "next/image";
+import Footer from '@/src/components/Footer';
+import Nav from '@/src/components/Nav';
 
 export default function Page() {
-  
-  const t = useTranslations('About');
-  return (
-  <>
-    <Link href="/">Back</Link>
-    <h1>{t('title')}</h1>
-    <Image
-    src="/ramone.png" 
-    width={280}
-    height={180}
-    alt="ramone picture"/>
 
-    <p>Test</p>
-  </>);
+  const t = useTranslations('About');
+
+  return (
+    <>
+      <container>
+        <Nav />
+        <Image
+          src="/ramone.png"
+          width={280}
+          height={180}
+          alt="ramone picture" />
+
+        {t.rich('about', {
+          paragraph: (p) => <p className="pt-2">{p}</p>,
+          italic: (i) => <em>{i}</em>
+        })}
+
+      </container>
+      <Footer />
+
+    </>);
 }
