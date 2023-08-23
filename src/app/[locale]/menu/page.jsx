0f78ecf8f42a4141c78cb9c76a54562a380728f9
egmp7 <deweyerick@gmail.com>
2023-08-22 19:27:44 -0500

    menu link updated

---
+++ src/app/[locale]/menu/page.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { images } from "./images";
+import { useState } from "react";
+
+export default function Gallery() {
+
+  const [index, setIndex] = useState();
+
+
+  return (
+    <>
+      <PhotoAlbum 
+      layout="rows" 
+      photos={images}
+      onClick={({ index }) => setIndex(index)} />
+
+      <Lightbox
+        plugins={[Captions, Thumbnails, Zoom]}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        slides={images}
+        
+      />
+    </>
+  )
+}
