2b67c9bc34f8c6b835e377d2cf2f7449d7de56e1
egmp7 <deweyerick@gmail.com>
2023-08-22 19:13:00 -0500

    title, description and thumbnail functionality lightbox

--- src/app/[locale]/gallery/page.jsx
+++ src/app/[locale]/gallery/page.jsx
@@ -2,15 +2,18 @@
 
 import { Gallery } from "react-grid-gallery";
 import Lightbox from "yet-another-react-lightbox";
+import { Captions, Thumbnails } from "yet-another-react-lightbox/plugins";
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 import { images } from "./images";
 import { useState } from "react";
 
 export default function GalleryImage() {
 
   const [openLightbox, setOpenLightbox] = useState(false);
-  const [index, setIndex] = useState(-1);
-  
+  const [index, setIndex] = useState();
+
   function handleClick (index) {
     setIndex(index) 
     setOpenLightbox(true)};
@@ -23,10 +26,12 @@ export default function GalleryImage() {
         onClick={ handleClick } />
 
       <Lightbox
+        plugins={[Captions, Thumbnails]}
         open={openLightbox}
         index={index}
         close={() => setOpenLightbox(false)}
         slides={images}
+        
       />
     </>
   )
