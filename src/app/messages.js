7a0ac1168e2016792a42583089da8c5975510bcf
egmp7 <deweyerick@gmail.com>
2023-08-24 13:13:57 -0500

    refactoring index page

---
+++ src/app/messages.js
@@ -0,0 +1,5 @@
+export const data ={
+    title: "Ramone Personal Chef",
+    slogan: "Chef for epicurean clients",
+    intro: "I am a personal chef with more than 15 years of experience, I studied and graduated from New York Culinary School in 1986. I also worked in New York at Vista International Hotel at World Trade Center in the 80’s and Ritz Carlton Hotels for 5 years. I have a extensive resume of very high end clientele including Robert Downey Jr, Jennifer López, Graig Robbins, Dj Tiesto and others."
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import IndexVisuals from '@/src/components/IndexVisuals';
+import { data } from './messages.js'
 
 export default function Index() {
 
@@ -6,16 +7,9 @@ export default function Index() {
     <>
       <main className='mt-4 lg:mt-16'>
         <IndexVisuals />
-        <h1 className="pt-4 text-3xl font-bold text-center">Ramone Personal Chef</h1>
-        <p className="pt-1 italic text-center">Chef for epicurean clients</p>
-        <p className="pt-2 px-2 text-justify sm:text-center md:mx-20">
-          Im a personal chef with more than 15 years of experience,
-          I studied and graduated from New York Culinary School in 1986.
-          I also worked in New York at Vista International Hotel at World Trade
-          Center in the 80’s and Ritz Carlton Hotels for 5 years.. I have a extensive
-          resume of very high end clientele including Robert Downey Jr. ,
-          Jennifer López, Graig Robbins, Dj Tiesto and others. Just go to my
-          website Personal Chef Ramone.</p>
+        <h1 className="pt-4 text-3xl font-bold text-center">{data.title}</h1>
+        <p className="pt-1 italic text-center">{data.slogan}</p>
+        <p className="pt-2 px-2 text-justify sm:text-center md:mx-20">{data.intro}</p>
           
       </main >
     </>
