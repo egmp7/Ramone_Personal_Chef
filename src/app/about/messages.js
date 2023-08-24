fc88eb571be60d9c029e9633e2f7a87a436cfaf5
egmp7 <deweyerick@gmail.com>
2023-08-24 13:53:06 -0500

    about refactoring messages.js

---
+++ src/app/about/messages.js
@@ -0,0 +1,19 @@
+export const data ={
+    title: "About Me",
+    subtitle: "High profile clients",
+    highProfile: "<i>Jennifer Lopez, Marc Anthony, Robert Downey Jr, DJ Tiesto, Market America CEO JR and Loren Ridinger, MIA Shoes CEO Richard Strauss, Actress Evelin Giro Mogul Realtor Craig Robbins, Peace Love World CEO Allina Villasante, NFL Quarterback Brady Quinn,</i> and many others.",
+    aboutMeText:[
+
+        "Chef Ramone's love affair for food began in his childhood, around family, watching how much passion and dedication can create such amazing meals using the best spices and flavors Cuban cuisine can offer.",
+
+        "When he realized that food, spices and cooking was his passion, he discovered that he was gifted in creating and developing complex and tasty dishes. Chef Ramone decided he needed to learn every secret and cooking technique in the culinary world.",
+        
+        "Chef Ramone enrolled in the culinary arts program at New York Culinary School and embarked on a culinary journey.  His first professional experience began as a chef at Vista International Hotel in the World Trade Center in New York.",
+
+        "Since then, he has dedicated years to traveling around the world to enrich the understanding and techniques cooking has to offer.  Preserving the authentic flavors and cooking methods passed down by generations of a myriad of cultures had become his true passion.  Chef Ramone has dedicated much time in perfecting these techniques.",
+
+        "Chef Ramone has also dedicated time to learn about American food (North, Central and South American).  He has studied the diverse and traditional cooking styles from Alaska to Argentina, working across the continent in some of the finest dining experiences available while representing the spirit of communities by using local ingredients. He has proudly learned and developed such unique techniques as a Chef working at the following establishments:",
+
+        "Hotel Valencia in San Diego, Elliot Bay Café in Seattle, Kincaids Restaurant, Marriott Hotel in Minneapolis, Hersaint and Cochon in New Orleans, and the Ritz Carlton in Miami."
+    ]
+}
